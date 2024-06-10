153035bc56e75546b58d7c6d30f0369ed9ead840
mahfooz <mahfooz720946@gmail.com>
2024-06-10 16:59:59 +0530

    fix: updated port configs acc to render
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app =express()
 const cors = require('cors');
 const utils = require("./utils/p-statement");
-
+const port = process.env.PORT || 4000;
 app.use(cors())
 app.use(express.json())
 
 app.get('/problem',(req, res)=>utils.getSubmissionCode(req,res));
-app.listen(5000);
+app.listen(port);
 
